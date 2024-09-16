daa6a807117a0a79ee9aedcd97d17df4689930be
emperorsixpacks <andrewdvd10@gmail.com>
2024-09-16 02:23:27 +0100

    updata: added eventbrite scrapper

--- src/scrapper/src/scrapper/base.py
+++ src/scrapper/src/scrapper/base.py
@@ -1,25 +1,53 @@
+import re
+from typing import Dict
+from urllib.parse import urljoin, urlencode
 from abc import ABC, abstractmethod
 from bs4 import BeautifulSoup
 
+from src.utils.session_manager import Session
+
 
 class BaseEventScrapper(ABC):
-    authenticate_url: str = None
-    has_authentication: bool = True
+    base_url: str = None
+    authentication_url: str = None
+    has_authentication: bool = False
+    headers: Dict[str, str] = {}
+
+    def __init__(self) -> None:
+        self.session = self._create_session()
+
     def __init_subclass__(cls) -> None:
         if not hasattr(cls, "__source_name__"):
             setattr(cls, "__source_name__", cls.__name__)
-        if hasattr(cls, "has_authentication") and getattr(cls, "has_authentication_url") is None:
+        if (
+            cls.has_authentication
+            and getattr(cls, "authentication_url") is None
+        ):
             raise ValueError("Provide a valid authentication url for your source")
-    
+
+    def _create_session(self) -> Session:
+        return Session(
+            headers=self.headers,
+        )
+
     def authenticate(self):
         pass
 
-    def parse_html(self, html_response, tag):
-        soup = BeautifulSoup(html_response, 'html.parser')
-        script_tags = soup.find_all('script')
-        pass
+    def parse_html(self, html_response, tag, regex:str = None):
+        soup = BeautifulSoup(html_response, "html.parser")
+        soup_result = soup.find_all(tag)
+        if regex:
+            soup_result = re.search(regex, soup_result).group(1)
+        return soup_result
+
+    def build_url(self, path: str = "", qparams=None):
+        url = urljoin(self.base_url, path)
+        if qparams:
+            query_string = urlencode(qparams)
+            url = f"{url}?{query_string}"
+
+        return url
 
     @abstractmethod
-    def scrape(self):
+    def scrape(self, path: str = None, qparams: Dict[str, str] = None):
         pass
-
--- src/scrapper/src/scrapper/eventBrite_scrapper.py
+++ src/scrapper/src/scrapper/eventBrite_scrapper.py
@@ -1,9 +1,19 @@
-import requests
 import re
+import json
+from typing import Dict
 from scrapper.base import BaseEventScrapper
 
+SERVER_DATA_REGEX = re.compile(r'__SERVER_DATA__\s*=\s*({.*?});', re.DOTALL)
+
 class EventBiteScrapper(BaseEventScrapper):
-    has_authentication = False
-    def scrape(self):
-        html_response = requests.get(self.authenticate_url).text
-        
+    base_url = "https://www.eventbrite.com"
+
+    def scrape(self, path: str = None, qparams: Dict[str, str] = None):
+        new_url = self.build_url(path, qparams)
+        response = self.session.get(new_url).text
+
+        parsed_html = self.parse_html(response, "script")[11].text
+        print(parsed_html)
+
+print("hello")
+    

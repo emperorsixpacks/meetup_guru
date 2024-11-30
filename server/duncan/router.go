9913ec3aaac22c4f9b84e94d113cd7f9e70b6f53
emperorsixpacks <andrewdvd10@gmail.com>
2024-11-30 22:25:30 +0100

    update: added request methods

--- server/duncan/router.go
+++ server/duncan/router.go
@@ -10,19 +10,23 @@ type Router struct {
 	r *mux.Router
 }
 
-func (this Router) GET(pattern string, handler func(res http.ResponseWriter, req *http.Request)) {
-	this.r.HandleFunc(pattern, handler).Methods("GET")
+// TODO look into the http.handler interface, I do not like passing this functions up and dan like this 
+
+func (this Router) GET(pattern string, handler func(res http.ResponseWriter, req *http.Request)) { 
+  this.r.HandleFunc(pattern, handler).Methods("GET")
 }
+
 func (this Router) POST(pattern string, handler func(res http.ResponseWriter, req *http.Request)) {
 	this.r.HandleFunc(pattern, handler).Methods("POST")
 }
-
-// Add an add route method and pass the verb explicity by default
-func (this Router) isValidRequestMethod(method string, r *http.Request) error {
-	if method != r.Method {
-		return ErrMethodNoAllowed
-	}
-	return nil
+func (this Router) PUT(pattern string, handler func(res http.ResponseWriter, req *http.Request)) {
+	this.r.HandleFunc(pattern, handler).Methods("PUT")
+}
+func (this Router) DELETE(pattern string, handler func(res http.ResponseWriter, req *http.Request)) {
+	this.r.HandleFunc(pattern, handler).Methods("DELETE")
+}
+func (this Router) AddMethod(request_method []string, pattern string, handler func(res http.ResponseWriter, req *http.Request))  {
+  this.r.HandleFunc(pattern, handler).Methods(request_method...)
 }
 
 func (this *Router) GetHandler() *mux.Router {

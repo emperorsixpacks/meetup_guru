6f5c8d50c6d63c5e22b3f3a3748158aae3e7bb83
emperorsixpacks <andrewdvd10@gmail.com>
2024-12-01 21:11:55 +0100

    update: working on routers and templates

--- server/server/server.go
+++ server/server/server.go
@@ -1,6 +1,19 @@
 package server
 
-func Run(){
-  // this function is what we will use to run the server based
-  // on what we have created
+import (
+	"meetUpGuru/m/duncan"
+	"net/http"
+)
+
+var (
+  DuncanServer = duncan.Defualt()
+  DuncanRouter = duncan.NewDuncanRouter()
+)
+
+func homePagehandler(res http.ResponseWriter, req *http.Request) {}
+
+func Run() {
+  DuncanServer.AddRouter(DuncanRouter)
+	// this function is what we will use to run the server based
+	// on what we have created
 }

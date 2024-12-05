a6654d656019ad19cf2e1b47efb43b84b9a853d3
emperorsixpacks <andrewdvd10@gmail.com>
2024-12-05 19:31:11 +0100

    mod: removed refernce in create user controller

--- server/controllers/userController.go
+++ server/controllers/userController.go
@@ -2,8 +2,8 @@ package controllers
 
 import "meetUpGuru/m/models"
 
-func CreateUserController(new_user *models.UserSchema) {
-	new_db_user := &models.User{UserSchema: *new_user}
+func CreateUserController(new_user models.UserSchema) {
+	new_db_user := &models.User{UserSchema: new_user}
 	baseDB.Create(new_db_user)
 
 }

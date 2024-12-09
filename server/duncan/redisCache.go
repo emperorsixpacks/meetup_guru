6ada9af538882009d59b5301f7d4e27d94113d57
emperorsixpacks <andrewdvd10@gmail.com>
2024-12-09 15:17:42 +0100

    mod: added redis get method

--- server/duncan/redisCache.go
+++ server/duncan/redisCache.go
@@ -1,18 +1,32 @@
 package duncan
 
-import "github.com/redis/go-redis/v9"
+import (
+	"context"
 
-type Redis
+	"github.com/redis/go-redis/v9"
+)
 
-type Redisclient struct{
-  rdb *redis.Client
+var ctx = context.Background() // I do not know, should I put this in the struct
+
+type RedisClient struct {
+	rdb *redis.Client
 }
 
-func NewredisClient(conn *RedisConnetion) *Redisclient{
-  options := redis.Options{
-    Addr: conn.Addr,
-    Password:  conn.Password,
-    DB: conn.DB, // Don't you think this is a bit repititve
-    
-  }
+func (this RedisClient) Get(key string) string {
+	val, err := this.rdb.Get(ctx,key).Result()
+	if err != nil {
+		panic(err)
+	}
+	return val // some type convertion has to come here because we are returning a string, but we are getting json
+
+}
+
+func newredisclient(conn RedisConnetion) *RedisClient {
+	options := &redis.Options{
+		Addr:     conn.Addr,
+		Password: conn.Password,
+		DB:       conn.DB,
+	}
+	client := redis.NewClient(options)
+	return &RedisClient{rdb: client}
 }

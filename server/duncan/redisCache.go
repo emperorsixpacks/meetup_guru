37e216b763a346272c259828bd108cf225668fd0
emperorsixpacks <andrewdvd10@gmail.com>
2024-12-10 16:31:00 +0100

    mod: added get data

--- server/duncan/redisCache.go
+++ server/duncan/redisCache.go
@@ -14,8 +14,8 @@ type RedisClient struct {
 	rdb *redis.Client
 }
 
-// this should return `json:""`
-func (this RedisClient) GetJSON(k string, o any) error {
+// TODO try to make this simpler
+func (this RedisClient) GetJSON(k string, o interface{}) error {
 	// NOTE this works
 	val, err := this.rdb.JSONGet(ctx, k).Result()
 	if err != nil {
@@ -29,7 +29,19 @@ func (this RedisClient) GetJSON(k string, o any) error {
 }
 
 // this should get json
-func (this RedisClient) Set(key string, value interface{}) {}
+func (this RedisClient) SetJSON(key string, value interface{}) error {
+	val, err := json.Marshal(value)
+	if err != nil {
+		return err
+	}
+	err = this.rdb.JSONSet(ctx, key,"$", val).Err()
+	if err != nil {
+		return err
+	}
+	return nil
+}
+func (this RedisClient) DeleteJSON(key string, value interface{}) {}
+func (this RedisClient) UpdateJSON(key string, value interface{}) {}
 
 // this should be private, and later, we should have only getconnection, var, should com from duncan config
 func NewRedisclient(conn RedisConnetion) (*RedisClient, error) {
